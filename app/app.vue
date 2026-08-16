f43e5ce9e0afd31ea7da55a6e63979676f7adafa
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-08-17 00:34:52 +0800

    chore: update app layout

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,22 @@
+<script setup lang="ts">
+  const { isSidebarCollapsed } = useLayoutStore();
+</script>
+
 <template>
   <UApp>
     <AppTopbar />
 
-    <main class="h-[calc(100svh-2.25rem)]">
-      <NuxtPage />
-    </main>
+    <div class="flex h-[calc(100svh-2.25rem)]">
+      <AppSidebar />
+
+      <AppScrollArea
+        :class="isSidebarCollapsed ? 'w-[calc(100svw-4.3125rem)]' : 'w-[calc(100svw-14.4375rem)]'"
+        as="main"
+        mount="both"
+        class="h-[calc(100svh-2.25rem)] transition-[width]"
+      >
+        <NuxtPage />
+      </AppScrollArea>
+    </div>
   </UApp>
 </template>
